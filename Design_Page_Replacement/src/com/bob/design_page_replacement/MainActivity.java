99bde41c8781a29f3822cedb6c975d517a8e1e65
Bob1993 <Bob1993@github.com>
2015-03-12 13:36:29 +0800

    完成基本功能

---
+++ Design_Page_Replacement/src/com/bob/design_page_replacement/MainActivity.java
@@ -0,0 +1,70 @@
+package com.bob.design_page_replacement;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.zhy.view.CircleMenuLayout;
+import com.zhy.view.CircleMenuLayout.OnMenuItemClickListener;
+
+/**
+ * <pre>
+ * @author bob
+ * </pre>
+ */
+public class MainActivity extends Activity {
+
+	private CircleMenuLayout mCircleMenuLayout;
+	private TextView current_array;
+
+	private String[] mItemTexts = new String[] { "OPT", "FIFO", "LRU" };
+	private int[] mItemImgs = new int[] { R.drawable.item_alg,
+			R.drawable.item_alg, R.drawable.item_alg };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main_activity);
+		current_array= (TextView) findViewById(R.id.current_array);
+		mCircleMenuLayout = (CircleMenuLayout) findViewById(R.id.id_menulayout);
+		mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);// 为布局传入图片和文字
+		mCircleMenuLayout// 设置监听事件
+				.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+					@Override
+					public void itemClick(View view, int pos) {
+						switch (pos) {
+						case 0:
+							Algorithm.opt(MainActivity.this);
+							break;
+						case 1:
+							Algorithm.fifo(MainActivity.this);
+							break;
+						case 2:
+							Algorithm.lru(MainActivity.this);
+							break;
+						}
+					}
+
+					@Override
+					public void itemCenterClick(View view) {
+						Intent intent = new Intent(MainActivity.this,
+								OptionActivity.class);
+						startActivity(intent);
+						finish();
+					}
+				});
+		show();
+		
+	}
+	
+	public void show()
+	{
+		current_array.setText("当前访问序列:\n"+Algorithm.strs);
+	}
+
+}
---
+++ Design_Page_Replacement/src/com/bob/design_page_replacement/OptionActivity.java
@@ -0,0 +1,68 @@
+package com.bob.design_page_replacement;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class OptionActivity extends Activity implements OnClickListener {
+	private EditText ed_input;
+	private Button bt_auto;
+	private Button bt_over;
+	private boolean isAuto = false;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.option_layout);
+
+		ed_input = (EditText) findViewById(R.id.ed_input);
+		bt_auto = (Button) findViewById(R.id.auto_arrays);
+		bt_over = (Button) findViewById(R.id.over);
+
+		bt_auto.setOnClickListener(this);
+		bt_over.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.auto_arrays: {
+			isAuto = true;// 设置标志位自动生成
+			auto_generation();
+		}
+			break;
+
+		case R.id.over: {
+			if (!isAuto) {// 非自动生成的序列
+
+				Algorithm.init(ed_input.getText().toString());
+			}
+
+			Intent intent = new Intent(OptionActivity.this, MainActivity.class);
+			startActivity(intent);
+			finish();// 结束当前活动
+		}
+		}
+	}
+
+	public void auto_generation()// 默认生成15位长的访问序列
+	{
+		String temp = "";
+		Random random = new Random(System.currentTimeMillis());// 利用系统时间来作为随机数的种子
+		for (int i = 0; i < 15; i++) {
+			temp += random.nextInt(10) + " ";
+		}
+		Algorithm.init(temp);// 初始化
+		Toast.makeText(OptionActivity.this, "已成功生成访问序列", 0).show();
+	}
+
+}
